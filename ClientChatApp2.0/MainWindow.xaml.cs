0e3635f36305fa919c5e79a3c99b75be34c12266
Muhammad Moiz <63865432+MoizMehmood333@users.noreply.github.com>
2022-12-28 15:35:10 +0500

    Add files via upload

---
+++ ClientChatApp2.0/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientChatApp2._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket socket;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] data = Encoding.Default.GetBytes(StringFromRichTextBox(txtSend));
+            socket.Send(BitConverter.GetBytes(data.Length), 0, 4 , 0 );
+            socket.Send(data);
+
+        }
+        string StringFromRichTextBox(RichTextBox rtb)
+        {
+            TextRange textRange = new TextRange(
+                rtb.Document.ContentStart,
+                rtb.Document.ContentEnd
+            );
+            return textRange.Text;
+        }
+
+        private void btnConnext_Click(object sender, RoutedEventArgs e)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000));
+            }
+            catch
+            {
+                MessageBox.Show("Unable To Connect");
+            }
+        }
+    }
+}
---
+++ ServerChatApp2.0/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace ServerChatApp2._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket mainSock;
+        Socket accSock;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListen_Click(object sender, RoutedEventArgs e)
+        {
+            mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            mainSock.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000));
+            mainSock.Listen(0);
+            accSock = mainSock.Accept();
+            mainSock.Close();
+
+            new Thread(() => {
+                while (true) {
+                    byte[] sizeBuffer = new byte[4];
+                    accSock.Receive(sizeBuffer, 0 , sizeBuffer.Length, SocketFlags.None);
+                    int size = BitConverter.ToInt32(sizeBuffer, 0);
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        txtResponse.Document.Blocks.Clear();
+                    });
+
+
+                    //this will hold the data for the buffers that we receive
+                    MemoryStream ms = new MemoryStream();
+;                   while (size > 0)
+                    {
+                        byte[] buffer;
+                        //size 30
+                        // buffersize 20
+
+                        if (size < accSock.ReceiveBufferSize)
+                        {
+                            buffer = new byte[size];
+                        }
+                        else {
+                            buffer = new byte[accSock.ReceiveBufferSize];
+                        }
+
+                        int receive = accSock.Receive(buffer);
+                        size -= receive; 
+                        ms.Write(buffer, 0, buffer.Length);
+                    }
+                    ms.Close();
+
+                    byte[] data = ms.ToArray();
+                    Application.Current.Dispatcher.Invoke(() => {
+                        txtResponse.Document.Blocks.Add(new Paragraph(new Run(Encoding.ASCII.GetString(data))));
+                    });
+
+                }
+            }).Start();
+        }
+        string StringFromRichTextBox(RichTextBox rtb)
+        {
+            TextRange textRange = new TextRange(
+                rtb.Document.ContentStart,
+                rtb.Document.ContentEnd
+            );
+            return textRange.Text;
+        }
+    }
+}
